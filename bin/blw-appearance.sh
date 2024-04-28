0666b0faa91c4947de6571e9659af073ce31e877
01micko <01micko#gmail.com>
2024-04-28 14:25:58 +1000

    blw-appearance.sh: fix path

--- bin/blw-appearance.sh
+++ bin/blw-appearance.sh
@@ -9,8 +9,8 @@ yad --title="Appearance Settings" \
   Here is where you can tune the appearance of your BunsenLabs desktop.\n
   Choose a wallpaper, an icon theme, a gtk theme or a cursor theme.\n" \
   --buttons-layout=center \
-  --button=" Wallpaper!preferences-desktop-wallpaper!choose wallpaper":'bash -c "./blw-wallpaper.sh &"' \
-  --button=" Icon Theme!preferences-desktop-icons!choose icons":'bash -c "./bl-icon-choice.sh &"' \
-  --button=" GTK Theme!preferences-desktop-theme!choose gtk theme":'bash -c "./bl-gtk-choice.sh &"' \
-  --button=" Cursor Theme!preferences-desktop-cursors!choose cursor style":'bash -c "./bl-cursor-choice.sh &"'
+  --button=" Wallpaper!preferences-desktop-wallpaper!choose wallpaper":'bash -c "blw-wallpaper.sh &"' \
+  --button=" Icon Theme!preferences-desktop-icons!choose icons":'bash -c "bl-icon-choice.sh &"' \
+  --button=" GTK Theme!preferences-desktop-theme!choose gtk theme":'bash -c "bl-gtk-choice.sh &"' \
+  --button=" Cursor Theme!preferences-desktop-cursors!choose cursor style":'bash -c "bl-cursor-choice.sh &"'
  
