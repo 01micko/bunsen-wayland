17072b46c2eba07fcfd6695d3848a2936fb9c2e5
01micko <01micko#gmail.com>
2024-05-06 16:27:58 +1000

    blw-appearance: add globals to main gui

--- bin/blw-appearance.sh
+++ bin/blw-appearance.sh
@@ -7,10 +7,11 @@ yad --title="Appearance Settings" \
   --window-icon=distributor-logo-bunsenlabs --name=distributor-logo-bunsenlabs \
   --image=distributor-logo-bunsenlabs --text "<big><big>Make a choice!</big></big>\n
   Here is where you can tune the appearance of your BunsenLabs desktop.\n
-  Choose a wallpaper, an icon theme, a gtk theme or a cursor theme.\n" \
+  Choose a wallpaper, an icon theme, gtk theme, cursor theme or a global theme.\n" \
   --buttons-layout=center \
   --button=" Wallpaper!preferences-desktop-wallpaper!choose wallpaper":'bash -c "blw-wallpaper.sh &"' \
   --button=" Icon Theme!preferences-desktop-icons!choose icons":'bash -c "bl-icon-choice.sh &"' \
   --button=" GTK Theme!preferences-desktop-theme!choose gtk theme":'bash -c "bl-gtk-choice.sh &"' \
-  --button=" Cursor Theme!preferences-desktop-cursors!choose cursor style":'bash -c "bl-cursor-choice.sh &"'
+  --button=" Cursor Theme!preferences-desktop-cursors!choose cursor style":'bash -c "bl-cursor-choice.sh &"' \
+  --button=" Global Theme!preferences-desktop-theme!choose a global theme for gtk, icons, wallpaper":'bash -c "blw-globals.sh &"'
  
